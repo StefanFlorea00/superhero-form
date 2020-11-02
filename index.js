54b9c3d214a5484275a76fa84bb4eb3287024373
Stefan <stefflr@yahoo.com>
2020-11-02 09:58:02 +0100

    updated js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const key = "5f9f25b9231ba42851b4a04d";
 const endpoint = "https://stefflr-0d51.restdb.io/rest/superheroes";
+
 _GET();
 
 function _GET() {
@@ -13,9 +14,9 @@ function _GET() {
   })
     .then((e) => e.json())
     .then(showHeroes)
-    .catch(function(error) {  
-    console.log('Request failed', error)  
-  });
+    .catch(function (error) {
+      console.log("Request failed", error);
+    });
 }
 
 function _POST(data) {
@@ -73,42 +74,68 @@ function showHeroes(list) {
   console.log(list);
 }
 
-document.querySelectorAll("button.main-btn").forEach(el => el.addEventListener("click", doNextSection));
-document.querySelectorAll("button.sec-btn").forEach(el => el.addEventListener("click", doPreviousSection));
+document.querySelectorAll("button.main-btn").forEach((el) => el.addEventListener("click", doNextSection));
+document.querySelectorAll("button.sec-btn").forEach((el) => el.addEventListener("click", doPreviousSection));
+document.querySelector("input[type=submit]").addEventListener("click", doSubmit);
+const form = document.querySelector("form");
+let activeSection = "#section-0";
+let activeSectionNode = document.querySelector(activeSection);
 
-function doNextSection(){
-  this.parentElement.parentElement.classList.add("hidden");
-  this.parentElement.parentElement.nextElementSibling.classList.toggle("hidden");
+function changeSection(newSection) {
+  console.log(activeSection);
+  activeSectionNode = document.querySelector(activeSection);
+  activeSectionNode.classList.add("hidden");
+  if (newSection) {
+    newSection.classList.toggle("hidden");
+  } else {
+    activeSection = "#section-0";
+    document.querySelector(activeSection).classList.toggle("hidden");
+  }
+  activeSection = "#" + newSection.id;
+}
 
-  if(this.parentElement.parentElement.nextElementSibling.id == "section-2"){
+function doNextSection() {
+  changeSection(this.parentElement.parentElement.nextElementSibling);
+
+  if (this.parentElement.parentElement.nextElementSibling.id == "section-2") {
     updateDetails();
   }
 }
 
-function doPreviousSection(){
-  this.parentElement.parentElement.classList.add("hidden");
-  console.log(this.parentElement.parentElement);
-  this.parentElement.parentElement.previousElementSibling.classList.toggle("hidden");
+function doSubmit() {
+  if (form.checkValidity()) {
+    changeSection(this.parentElement.parentElement.nextElementSibling);
+  } else {
+    changeSection(document.querySelector("#section-0"));
+  }
+
+  document.querySelectorAll("input").forEach((el) => (el.value = ""));
 }
 
-function updateDetails(){
+function doPreviousSection() {
+  changeSection(this.parentElement.parentElement.previousElementSibling);
+}
+
+function updateDetails() {
   const details = document.querySelector(".details-wrapper");
   const detailsRight = document.querySelectorAll(".details-wrapper .right p");
   console.log(form.elements);
-  detailsRight[0].textContent = form.elements.hero_name.value;
-  detailsRight[1].textContent = form.elements.real_name.value;
-  detailsRight[2].textContent = form.elements.age.value;
-  detailsRight[3].textContent = form.elements.gender.value;
-  detailsRight[4].textContent = form.elements.home_planet.value;
-  // not getting strengths despite getting weaknesses???
-  // detailsRight[5].textContent = form.elements.strengths.value;
-  detailsRight[6].textContent = form.elements.weaknesses.value;
-  detailsRight[7].textContent = form.elements.team.value;
+
+  changeDetails("hero_name", form.elements.age.value);
+  changeDetails("real_name", form.elements.real_name.value);
+  changeDetails("age", form.elements.age.value);
+  changeDetails("gender", form.elements.gender.value);
+  changeDetails("home_planet", form.elements.home_planet.value);
+  changeDetails("strengths", form.elements.strengths.value);
+  changeDetails("weaknesses", form.elements.weaknesses.value);
+  changeDetails("team", form.elements.team.value);
 }
 
-
-
-const form = document.querySelector("form");
+function changeDetails(datafield, text) {
+  if (text == "") {
+    document.querySelector(`[data-field=${datafield}]`).textContent = "-";
+  } else document.querySelector(`[data-field=${datafield}]`).textContent = text;
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
